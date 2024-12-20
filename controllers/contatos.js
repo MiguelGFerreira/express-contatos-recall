7bccbf771d5113ffaeecfb2c7dec0efba6d81a89
miguel <miguel.gfe@outlook.com>
2024-12-20 13:10:39 -0300

    feat: adds patch and delete endpoints

--- controllers/contatos.js
+++ controllers/contatos.js
@@ -9,12 +9,101 @@ export const getContatos = (req, res) => {
 				AND S.MATRICULA = CAST(C.MATRICULA AS INT)
 				)
 		WHERE ISNULL(S.DATA_DEMISSAO, '') = ''
+			AND C.DELETADO = 0
 		ORDER BY C.TIPO, C.CONTATO
 	`;
 
 	new sql.Request().query(query, (err, result) => {
 		if (err) {
-			console.error("Error executing query:", err);
+			console.error("Error executing getContatos: ", err);
+			console.error("Query: ", query);
+		} else {
+			res.send(result.recordset); // Send query result as response
+		}
+	});
+}
+
+export const postContatos = (req, res) => {
+	let query = `
+		INSERT INTO RKF_CONTATOS_RECALL (
+			CONTATO
+			,MATRICULA
+			,SETOR
+			,TELEFONE
+			,EMAIL
+			,SITE
+			,TIPO
+			,DELETADO
+			)
+		VALUES (
+			'${req.body.contato}'
+			,'${req.body.matricula}'
+			,'${req.body.setor}'
+			,'${req.body.telefone}'
+			,'${req.body.email}'
+			,'${req.body.site}'
+			,'${req.body.tipo}'
+			,0
+			)
+	`;
+
+	console.log(query);
+
+	new sql.Request().query(query, (err, result) => {
+		if (err) {
+			console.error("Error executing postContatos: ", err);
+			console.error("Query: ", query);
+		} else {
+			res.send(result.recordset); // Send query result as response
+		}
+	});
+}
+
+export const getFuncionarios = (req, res) => {
+	let query = `
+		SELECT RIGHT(replicate('0',6) + CAST(MATRICULA AS VARCHAR),6) matricula,
+			TRIM(NOME) nome 
+		FROM FOLHA12..SENIOR_COLABORADOR
+		WHERE DATA_DEMISSAO = ''
+		ORDER BY NOME
+		`
+
+	new sql.Request().query(query, (err, result) => {
+		if (err) {
+			console.error("Error executing getFuncionarios: ", err);
+			console.error("Query: ", query);
+		} else {
+			res.send(result.recordset); // Send query result as response
+		}
+	});
+}
+
+export const patchContato = (req, res) => {
+	const { contato, matricula, setor, telefone, email, site, deletado } = req.body;
+
+	let query = "";
+
+	if (deletado == 1) {
+		query = `UPDATE RKF_CONTATOS_RECALL SET DELETADO = 1 WHERE ID = ${req.params.idcontato}`;
+	} else {
+		query = `
+		UPDATE RKF_CONTATOS_RECALL
+		SET CONTATO = '${contato}'
+			,MATRICULA = '${matricula}'
+			,SETOR = '${setor}'
+			,TELEFONE = '${telefone}'
+			,EMAIL = '${email}'
+			,SITE = '${site}'
+		WHERE ID = ${req.params.idcontato}
+	`
+	}
+
+	console.log(query);
+
+	new sql.Request().query(query, (err, result) => {
+		if (err) {
+			console.error("Error executing patchContato: ", err);
+			console.error("Query: ", query);
 		} else {
 			res.send(result.recordset); // Send query result as response
 		}
--- routes/contatos.js
+++ routes/contatos.js
@@ -1,9 +1,12 @@
 import express from 'express';
 
-import { getContatos } from '../controllers/contatos.js'
+import { getContatos, getFuncionarios, patchContato, postContatos } from '../controllers/contatos.js'
 
 const router = express.Router();
 
 router.get('/', getContatos);
+router.post('/', postContatos);
+router.patch('/:idcontato', patchContato);
+router.get('/funcionarios', getFuncionarios);
 
 export default router;
